dae375f8d89b8f9a67e96ec04783bae721233aa7
Michal Przysucha <mprzysucha@yahoo.com>
2016-03-07 23:53:08 +0100

    Problem 64

--- src/main/scala/projecteuler/problem64/Problem64.scala
+++ src/main/scala/projecteuler/problem64/Problem64.scala
@@ -1,9 +1,38 @@
 package projecteuler.problem64
 
+import scala.math.sqrt
+
 /**
   * @author Michal Przysucha
-  * @since 05.03.2016 22:20:00
   */
-class Problem64 {
+object Problem64 extends App {
+
+
+  val periods = for (n <- 2 to 10000 if (sqrt(n) % 1 != 0)) yield find(n)
+
+  println(periods.toList.filter(x => x % 2 != 0).size)
+
+  def find(n: Long): Int = {
+    var m = Map.empty[Triple, Int]
+    for (t <- triples(n))
+      if (m contains t) return m get t get
+      else m = (m + (t -> 0)).mapValues(x => x + 1)
+    return 0
+  }
+
+  type Triple = Tuple3[Long, Long, Long]
+
+  def triples(n: Long): Stream[Triple] = {
+    val s1 = sqrt(n).toLong
+    triples(n, s1, new Triple(s1, 0, 1))
+  }
+
+  def triples(n: Long, s1: Long, t: Triple): Stream[Triple] = {
+    val newA = -t._2 + t._1 * t._3
+    val newB = (n - newA * newA) / t._3
+    val newS = (s1 + newA) / newB
+    val newT = new Triple(newS, newA, newB)
+    Stream.cons(t, triples(n, s1, newT))
+  }
 
 }
